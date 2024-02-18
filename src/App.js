474254325ddd9318c7c42c6c3dd75bb90363653d
Visal20497 <visal20497@gmail.com>
2024-02-19 01:19:40 +0530

    Redux

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+import Counter from './component/Counter'
+import Todo from './component/Todo'
+ 
+ function App() {
+   return (
+      // <Counter/>
+      <Todo/>
+   )
+ }
+ 
+ export default App
